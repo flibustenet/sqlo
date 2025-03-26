c6d7277f6c0f08258799772be1402921738b849d
Wilk <wilk@flibuste.net>
2025-03-26 08:14:23 +0100

    maj tests

--- sql_test.go
+++ sql_test.go
@@ -13,12 +13,11 @@ import (
 )
 
 func Test_insertSt(t *testing.T) {
-	tx := &Sx{}
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
 	fs["raw"] = Raw("now()")
-	q, args := tx.insertSt("mytable", fs)
+	q, args := insertSt(DB_PG, "mytable", fs)
 	if q != "INSERT INTO mytable (ok, raw, yes) VALUES ($1, now(), $2)" {
 		t.Error(q)
 		t.Error(args)
@@ -26,12 +25,11 @@ func Test_insertSt(t *testing.T) {
 }
 
 func Test_updateSt(t *testing.T) {
-	tx := &Sx{}
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
 	fs["raw"] = Raw("now()")
-	q, args := tx.updateSt("mytable", fs, "ok=$1", "ok")
+	q, args := updateSt(DB_PG, "mytable", fs, "ok=$1", "ok")
 	if q != "UPDATE mytable SET ok=$2, raw=now(), yes=$3 WHERE ok=$1" {
 		t.Error(q)
 		t.Error(args)
@@ -39,49 +37,40 @@ func Test_updateSt(t *testing.T) {
 }
 
 func Test_insertStMssql(t *testing.T) {
-	tx := &Sx{}
-	tx.DbType = DB_MSSQL
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
-	q, _ := tx.insertSt("mytable", fs)
+	q, _ := insertSt(DB_MSSQL, "mytable", fs)
 	if q != "INSERT INTO mytable (ok, yes) VALUES (@p1, @p2)" {
 		log.Fatal(q)
 	}
 }
 
 func Test_updateStMssql(t *testing.T) {
-	tx := &Sx{}
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
-	tx.DbType = DB_MSSQL
-	q, _ := tx.updateSt("mytable", fs, "ok=@p1", "ok")
+	q, _ := updateSt(DB_MSSQL, "mytable", fs, "ok=@p1", "ok")
 	if q != "UPDATE mytable SET ok=@p2, yes=@p3 WHERE ok=@p1" {
 		log.Fatal(q)
 	}
 }
 
 func Test_insertStAccess(t *testing.T) {
-	tx := &Sx{}
-	tx.DbType = DB_ACCESS
-
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
-	q, _ := tx.insertSt("mytable", fs)
+	q, _ := insertSt(DB_ACCESS, "mytable", fs)
 	if q != "INSERT INTO mytable (ok, yes) VALUES (?, ?)" {
 		log.Fatalf("insert access : %s", q)
 	}
 }
 
 func Test_updateStAccess(t *testing.T) {
-	tx := &Sx{}
-	tx.DbType = DB_ACCESS
-	fs := map[string]interface{}{}
+	fs := map[string]any{}
 	fs["ok"] = "coral"
 	fs["yes"] = "no"
-	q, _ := tx.updateSt("mytable", fs, "ok=?", "ok")
+	q, _ := updateSt(DB_ACCESS, "mytable", fs, "ok=?", "ok")
 	if q != "UPDATE mytable SET ok=?, yes=? WHERE ok=?" {
 		log.Fatalf("update access : %s", q)
 	}
--- where_test.go
+++ where_test.go
@@ -15,14 +15,14 @@ func TestWhereDol(t *testing.T) {
 	}
 	d := D{"a=%s and b=%s", []int{1, 2}, " where a=$1 and b=$2"}
 	where := &Where{}
-	where.Appendf("a=%s", 1)
-	where.Appendf("b=%s", 2)
+	where.And("a=%s", 1)
+	where.And("b=%s", 2)
 	res := where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
 	}
 	where = &Where{}
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -30,8 +30,8 @@ func TestWhereDol(t *testing.T) {
 
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=x and a=$1 and b=$2"}
 	where = &Where{}
-	where.Appendf("x=x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -39,9 +39,9 @@ func TestWhereDol(t *testing.T) {
 
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=$1 and a=$2 and b=$3"}
 	where = &Where{}
-	where.Appendf("x=$1")
-	where.Appendf("", "x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=$1")
+	where.And("", "x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -49,9 +49,9 @@ func TestWhereDol(t *testing.T) {
 
 	d = D{"a=%s and b in (%s,%s) and c=%s", []int{1, 2, 3, 4}, " where a=$1 and b in ($2,$3) and c=$4"}
 	where = &Where{}
-	where.Appendf("a=%s", 1)
-	where.AppendListf("b in (%s)", 2, 3)
-	where.Appendf("c=%s", 4)
+	where.And("a=%s", 1)
+	where.AndList("b in (%s)", 2, 3)
+	where.And("c=%s", 4)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -66,15 +66,15 @@ func TestWhereSQLServer(t *testing.T) {
 	d := D{"a=%s and b=%s", []int{1, 2}, " where a=@p1 and b=@p2"}
 	where := &Where{}
 	where.Style = "@p"
-	where.Appendf("a=%s", 1)
-	where.Appendf("b=%s", 2)
+	where.And("a=%s", 1)
+	where.And("b=%s", 2)
 	res := where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
 	}
 	where = &Where{}
 	where.Style = "@p"
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -83,8 +83,8 @@ func TestWhereSQLServer(t *testing.T) {
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=x and a=@p1 and b=@p2"}
 	where = &Where{}
 	where.Style = "@p"
-	where.Appendf("x=x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -93,9 +93,9 @@ func TestWhereSQLServer(t *testing.T) {
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=@p1 and a=@p2 and b=@p3"}
 	where = &Where{}
 	where.Style = "@p"
-	where.Appendf("x=@p1")
-	where.Appendf("", "x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=@p1")
+	where.And("", "x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -104,9 +104,9 @@ func TestWhereSQLServer(t *testing.T) {
 	d = D{"a=%s and b in (%s,%s) and c=%s", []int{1, 2, 3, 4}, " where a=@p1 and b in (@p2,@p3) and c=@p4"}
 	where = &Where{}
 	where.Style = "@p"
-	where.Appendf("a=%s", 1)
-	where.AppendListf("b in (%s)", 2, 3)
-	where.Appendf("c=%s", 4)
+	where.And("a=%s", 1)
+	where.AndList("b in (%s)", 2, 3)
+	where.And("c=%s", 4)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -121,15 +121,15 @@ func TestWhereQMark(t *testing.T) {
 	d := D{"a=%s and b=%s", []int{1, 2}, " where a=? and b=?"}
 	where := &Where{}
 	where.Style = "?"
-	where.Appendf("a=%s", 1)
-	where.Appendf("b=%s", 2)
+	where.And("a=%s", 1)
+	where.And("b=%s", 2)
 	res := where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
 	}
 	where = &Where{}
 	where.Style = "?"
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -138,8 +138,8 @@ func TestWhereQMark(t *testing.T) {
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=x and a=? and b=?"}
 	where = &Where{}
 	where.Style = "?"
-	where.Appendf("x=x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -148,9 +148,9 @@ func TestWhereQMark(t *testing.T) {
 	d = D{"a=%s and b=%s", []int{1, 2}, " where x=? and a=? and b=?"}
 	where = &Where{}
 	where.Style = "?"
-	where.Appendf("x=?")
-	where.Appendf("", "x")
-	where.Appendf("a=%s and b=%s", 1, 2)
+	where.And("x=?")
+	where.And("", "x")
+	where.And("a=%s and b=%s", 1, 2)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
@@ -159,9 +159,9 @@ func TestWhereQMark(t *testing.T) {
 	d = D{"a=%s and b in (%s,%s) and c=%s", []int{1, 2, 3, 4}, " where a=? and b in (?,?) and c=?"}
 	where = &Where{}
 	where.Style = "?"
-	where.Appendf("a=%s", 1)
-	where.AppendListf("b in (%s)", 2, 3)
-	where.Appendf("c=%s", 4)
+	where.And("a=%s", 1)
+	where.AndList("b in (%s)", 2, 3)
+	where.And("c=%s", 4)
 	res = where.Where()
 	if res != d.query {
 		t.Errorf("de %s %v attend %s reçoit %s", d.sql, d.args, d.query, res)
